e23ac7fc7060dd0b0a52781a93475eb7f0ea2c17
Guilherme Monteiro <guivlm.sp@gmail.com>
2024-11-05 02:47:59 +0000

    Adding `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 function Home() {
-    return <h1>Amor, tudo isso vai passar, é só uma questão de tempo e paciência! Te Amo!</h1>;
+  return (
+    <h1>
+      Amor, tudo isso vai passar, é só uma questão de tempo e paciência! Te Amo!
+    </h1>
+  );
+}
+
+function test1() {
+  return console.log("teste");
+}
+
+function test2() {
+  return console.log("teste");
+}
+
+function test3() {
+  return console.log("teste");
 }
 
 export default Home;
